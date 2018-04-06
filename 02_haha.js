4777430a7fd5a8e6d25eb728754cf934dc9032d8
viktor gopanchuk <vitugan@gmail.com>
2018-04-07 00:18:56 +0300

    Done 02

---
+++ 02_haha.js
@@ -0,0 +1,14 @@
+/*
+Write a function that implements Pythagorean theorem.
+*/
+
+function pythagoreanTheorem(){
+	let a = parseInt(prompt('Input a:'));
+	let b = parseInt(prompt('Input b:'));
+	if ( isNaN(a) || isNaN(b)) {
+		alert("Input a valid number and try again.")
+		return;
+	}	
+	c = Math.sqrt(a*a + b*b);
+	return c;
+}
