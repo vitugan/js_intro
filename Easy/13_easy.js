7b8a4d55f2a71726a176a332045949441970baa1
viktor gopanchuk <vitugan@gmail.com>
2018-04-08 22:38:54 +0300

    Done task 2 Easd

---
+++ Easy/13_easy.js
@@ -0,0 +1,13 @@
+/**
+ * Write a function that returns average number of values. Do not use Math. For i.e. [1,4,2] should return [2.33].
+ */
+
+ function average_num(arr){
+    var sum = arr.reduce(function(prev, current){
+        return prev + current;
+    });
+    var average = Math.round((sum / arr.length) * 100) / 100;
+    return average;
+ }
+
+ average_num([1,4,2]);
