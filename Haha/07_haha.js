dd1c80f4f39c9c882423bef7c7e84cc6fd4af85e
viktor gopanchuk <vitugan@gmail.com>
2018-04-08 22:01:18 +0300

    Done HAHA level

---
+++ Haha/07_haha.js
@@ -0,0 +1,9 @@
+/**
+ * Write a JavaScript function to convert radians to degrees. Sample
+ */
+
+function radians_to_degrees(rad){
+    return (rad * 180) / Math.PI;
+}
+
+console.log(radians_to_degrees(0.7853981633974483));
