94e53ceae4072e9e58c9dbd0c805c028c582684c
viktor gopanchuk <vitugan@gmail.com>
2018-04-14 14:21:53 +0300

    Task 26 done

---
+++ Medium/26_medium.js
@@ -0,0 +1,14 @@
+/**
+ * Write a JavaScript program to calculate the factorial of a number. Sample f(5) should return 120.
+ */
+
+ function factorial(n){
+    var res = n;
+    for(var i = n - 1; i > 0; i--){
+        res = res * i;
+        debugger
+    }
+     return res;
+ }
+
+ console.log(factorial(3));
