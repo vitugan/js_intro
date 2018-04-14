1f2b1fb05f44d95aac30951c48241d742bad2a80
viktor gopanchuk <vitugan@gmail.com>
2018-04-14 22:43:53 +0300

    Task 34 done

--- Hard/33_hard.js
+++ Hard/33_hard.js
@@ -1,17 +1,25 @@
 /**
- * Write a JavaScript program to find duplicate values in a JavaScript array.
+ * Read about Binary Search. 
+ * Implement it using JavaScript. 
+ * For i.e. binarySearch(27, {from:0, to:100}), 
+ * should print at console log 50,25,38,31,28,27.
  */
-
-function get_duplicates(arr){
-    var duplicates = [];
-    arr.sort();
-    for(var i=0;i<arr.length;i++){
-        if(arr[i] === arr[i-1]){
-            duplicates.push(arr[i]);
-        }
-    }
-    return duplicates;
+function biynarySearch(n, range){
+    var start = range.from, 
+        end = range.to;    
+    
+    while(start<=end){
+        debugger
+        var middle = Math.ceil((end+start) / 2);
+        console.log(middle);
+        if(n === middle){            
+            return;
+        } else if(middle > n){
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }        
+    }    
 }
 
-var arr = ["name", 22, 1, 22, false, true, "name", false];
-console.log(get_duplicates(arr));
+biynarySearch(27, {from: 0,to: 100});
